a0d3e868a23658223731a8818383a03373424d3f
Ryan Campbell <ryan@rcampbell.xyz>
2022-04-09 10:10:47 -0500

    working on setenv

--- cmd/setenv.go
+++ cmd/setenv.go
@@ -22,10 +22,13 @@ THE SOFTWARE.
 package cmd
 
 import (
-	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
+
+var directory string
+var wallPath string
 
 // setenvCmd represents the setenv command
 var setenvCmd = &cobra.Command{
@@ -33,22 +36,23 @@ var setenvCmd = &cobra.Command{
 	Short: "Set env values for settings",
 	Long: `wp setenv - Can be set via shell or using this command
 Sets the individual env values for each configurable setting.
-	 wp setenv -d [directory]		set location for random wallpaper selection`,
+	wp setenv -d [directory]		set location for random wallpaper selection
+	wp setend -w [wallpaper path]  set location to save wallpaper`,
+
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("setenv called")
+		if directory != "" {
+			os.Setenv("WP_DIR", directory)
+		}
+
+		if wallPath != "" {
+			os.Setenv("WP_WALL_PATH", wallPath)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(setenvCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// setenvCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// setenvCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	setenvCmd.Flags().StringVarP(&directory, "directory", "d", "", "Dir to use for random wallpaper selection")
+	setenvCmd.Flags().StringVarP(&wallPath, "wallpath", "w", "", "Place to save wallpaper file")
 }
